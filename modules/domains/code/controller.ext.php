433ced347ae28e923c6b00010a0b21e68f30fbc4
Automatisa <Compras@automatisa.com>
2026-06-23 11:28:05 +0200

    Implement new home directory creation and permissions
    Add functionality to create a new home directory and set permissions.

--- modules/domains/code/controller.ext.php
+++ modules/domains/code/controller.ext.php
@@ -148,6 +148,7 @@ class module_controller extends ctrl_module
             if ($autohome == 1) {
                 $destination = str_replace(".", "_", $domain);
                 $vhost_path = ctrl_options::GetSystemOption('hosted_dir') . $currentuser['username'] . "/public_html/" . $destination . "/";
+                fs_director::CreateDirectory(trim($vhost_path, '/'));
                 fs_director::CreateDirectory($vhost_path);
                 fs_director::SetFileSystemPermissions($vhost_path, 0777);
                 //** Existing Home Directory **//
